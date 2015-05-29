3be8ee9c97d8e4050af553a80c11d2aaf5fa13ac
César Lara <laralacesar@gmail.com>
2015-05-29 12:31:36 -0500

    It is possible to get location, send email and do it every ten seconds. CODE IS DEEPLY AWFUL.

--- Practica25_Servicios/app/src/main/java/mx/ipn/edu/practica25_servicios/MainActivity.java
+++ Practica25_Servicios/app/src/main/java/mx/ipn/edu/practica25_servicios/MainActivity.java
@@ -1,30 +1,71 @@
 package mx.ipn.edu.practica25_servicios;
 
-import android.app.Activity;
-import android.app.NotificationManager;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    // LogCat tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
-//    private NotificationManager nm;
-//    private static final int ID_NOTIFICACION_CREAR = 1;
+    private Location mLastLocation;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+
+    private LocationRequest mLocationRequest;
+
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 5000; // 5 sec
+    private static int DISPLACEMENT = 10; // 10 meters
+
+    // UI elements
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // First we need to check availability of play services
+        if (checkPlayServices()) {
+
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
+
 
 //        nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
         Button arrancar = (Button) findViewById(R.id.boton_arrancar);
         arrancar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                startService(new Intent(MainActivity.this,
-                        ServicioMusica.class));
+                double[] location =  getLocation();
+                double latitude = location[0];
+                double longitude = location[1];
+                Intent servicioMusica = new Intent(MainActivity.this,
+                        ServicioMusica.class);
+                servicioMusica.putExtra("latitude", latitude);
+                servicioMusica.putExtra("longitude", longitude);
+                startService(servicioMusica);
             }
         });
         Button detener = (Button) findViewById(R.id.boton_detener);
@@ -34,5 +75,89 @@ public class MainActivity extends Activity {
                         ServicioMusica.class));
             }
         });
+
+    }
+
+    public double[] getLocation() {
+
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+            double[] location = {latitude, longitude};
+
+            return location;
+
+        }
+        return null;
+    }
+
+    /**
+     * Creating google api client object
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    /**
+     * Method to verify google play services on the device
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkPlayServices();
+    }
+
+    /**
+     * Google api callback methods
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+
+        // Once connected with google api, get the location
+        //displayLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
     }
 }
--- Practica25_Servicios/app/src/main/java/mx/ipn/edu/practica25_servicios/ServicioMusica.java
+++ Practica25_Servicios/app/src/main/java/mx/ipn/edu/practica25_servicios/ServicioMusica.java
@@ -6,59 +6,91 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ServicioMusica extends Service {
     MediaPlayer reproductor;
     private NotificationManager nm;
     private static final int ID_NOTIFICACION_CREAR = 1;
+    ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
     @Override
     public void onCreate() {
         nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         Toast.makeText(this, "Servicio creado",
                 Toast.LENGTH_SHORT).show();
-        reproductor = MediaPlayer.create(this, R.raw.audio);
     }
 
-//    START_STICKY: Cuando sea posible el sistema tratará de recrear el servicio, se realizará una llamada a onStartCommand() pero con el parámetro intencion igual a null. Esto tiene sentido cuando el servicio puede arrancar sin información adicional, como por ejemplo, el servicio mostrado para la reproducción de música de fondo.
-//    START_NOT_STICKY: El sistema no tratará de volver a crear el servicio, por lo tanto el parámetro intencion nunca podrá ser igual a null. Esto tiene sentido cuando el servicio no puede reanudarse una vez interrumpido.
-//    START_REDELIVER_INTENT: El sistema tratará de volver a crear el servicio. El parámetro intencion será el que se utilizó en la última llamada startService(Intent).
+//    START_STICKY: Cuando sea posible el sistema tratarï¿½ de recrear el servicio, se realizarï¿½ una llamada a onStartCommand() pero con el parï¿½metro intencion igual a null. Esto tiene sentido cuando el servicio puede arrancar sin informaciï¿½n adicional, como por ejemplo, el servicio mostrado para la reproducciï¿½n de mï¿½sica de fondo.
+//    START_NOT_STICKY: El sistema no tratarï¿½ de volver a crear el servicio, por lo tanto el parï¿½metro intencion nunca podrï¿½ ser igual a null. Esto tiene sentido cuando el servicio no puede reanudarse una vez interrumpido.
+//    START_REDELIVER_INTENT: El sistema tratarï¿½ de volver a crear el servicio. El parï¿½metro intencion serï¿½ el que se utilizï¿½ en la ï¿½ltima llamada startService(Intent).
 
     @Override
     public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        Bundle bundle = intenc.getExtras();
+        final Double latitude = bundle.getDouble("latitude");
+        final Double longitude = bundle.getDouble("longitude");
 
         Toast.makeText(this,"Servicio arrancado "+ idArranque,
                 Toast.LENGTH_SHORT).show();
 
         Notification notificacion = new Notification(
                 R.mipmap.ic_launcher,
-                "Creando Servicio de Música",
+                "Creando Servicio de Mï¿½sica",
                 System.currentTimeMillis() );
 
         PendingIntent intencionPendiente = PendingIntent.getActivity(
                 this, 0, new Intent(this, MainActivity.class), 0);
-        notificacion.setLatestEventInfo(this, "Reproduciendo música",
-                "información adicional", intencionPendiente);
+        notificacion.setLatestEventInfo(this, "Enviando Correo",
+                "informaciï¿½n adicional", intencionPendiente);
 
+
+        Runnable helloRunnable = new Runnable() {
+            public void run() {
+                String mail = "laralacesar@gmail.com";
+                String subject = "We love Pertica";
+                String message = "Latutude: " + String.valueOf(latitude) +
+                        "Longitude: " + String.valueOf(longitude);
+                SendEmail(mail, subject, message);
+            }
+        };
+
+        executor.scheduleAtFixedRate(helloRunnable, 0, 10, TimeUnit.SECONDS);
         nm.notify(ID_NOTIFICACION_CREAR, notificacion);
 
-
-        reproductor.start();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
+        executor.shutdownNow();
         nm.cancel(ID_NOTIFICACION_CREAR);
         Toast.makeText(this,"Servicio detenido",
                 Toast.LENGTH_SHORT).show();
-        reproductor.stop();
     }
 
     @Override
     public IBinder onBind(Intent intencion) {
         return null;
+    }
+
+    public void SendEmail(String mail, String subject, String message){
+        Log.d("Send email", mail);
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{mail});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+        emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(emailIntent);
     }
 }
